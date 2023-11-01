f6480a4063e52837bdc23b4b312925d75753b1c1
Alexandra Navarro <alexandra.navarro@usach.cl>
2023-11-01 17:16:19 -0300

    comienzo proyecto

---
+++ Microservicios/backendconfig-service/src/main/java/mingeso/backendconfigservice/BackendconfigServiceApplication.java
@@ -0,0 +1,16 @@
+package mingeso.backendconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BackendconfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendconfigServiceApplication.class, args);
+	}
+
+}
