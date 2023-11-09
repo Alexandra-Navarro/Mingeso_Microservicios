3eed6a86a181199bb7560636180059b5f03d436b
Alexandra Navarro <alexandra.navarro@usach.cl>
2023-11-09 09:38:16 -0300

    Termino de estudiantes

--- Microservicios/backend-estudiantes-service/src/main/java/backendestudiantesservice/backendestudiantesservice/entity/EstudianteEntity.java
+++ Microservicios/backend-estudiantes-service/src/main/java/backendestudiantesservice/backendestudiantesservice/entity/EstudianteEntity.java
@@ -1,11 +1,10 @@
 package backendestudiantesservice.backendestudiantesservice.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
-
+import javax.persistence.*;
 @Entity
 @Table(name = "estudiantes")
 @Data
--- Microservicios/backend-gateway-service/src/main/java/mingeso/backendgatewayservice/BackendGatewayServiceApplication.java
+++ Microservicios/backend-gateway-service/src/main/java/mingeso/backendgatewayservice/BackendGatewayServiceApplication.java
@@ -3,11 +3,10 @@ package mingeso.backendgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class BackendGatewayServiceApplication {
 
 	public static void main(String[] args) {
